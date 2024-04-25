8a743d0e7fad755bd351990d029120f855d520b0
Emily Durie <edurie@gatech.edu>
2024-04-24 21:28:34 -0600

    Added and debugged a bit of Weather API

--- References.md
+++ References.md
@@ -2,7 +2,9 @@ References Used throughout development:
 
 
 Starter Code: https://realpython.com/python-shebang/
+Command Line Arguments: https://realpython.com/python-command-line-arguments/
 
+VSCode Python Debugging with arguments: https://code.visualstudio.com/docs/python/debugging#:~:text=Specifies%20arguments%20to%20pass%20to,to%20%24%7Bcommand%3ApickArgs%7D%20.
 
 Tools Used during Development: 
 - Black Formater for Python
--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
+import requests
+import sys
+
+
+# Requires: Lat and Long Data
+def getWeatherAPIInfo(my_api_key, p_lat, p_lon):
+    # p_lat = latitude value of the earthquake
+    # p_lon = longitude value of the earthquake
+
+    # my_api_key = the API key obtained with my free account
+    method = "GET"
+    url_base = "https://api.weatherapi.com/v1/current.json"
+    pars = {"q": f"{p_lat},{p_lon}", "key": my_api_key}
+    response_json = requests.request(method=method, url=url_base, params=pars).json()
+    temp_f = response_json["current"]["temp_f"]
+    print(temp_f)
+
+
+def getEarthquakeAPI():
+    pass
 
 
 if __name__ == "__main__":
-    print("This is an earthquake solution")
+    if len(sys.argv) < 3:
+        print(
+            f"Not enough arguments passed in, only saw {len(sys.argv)}: must provide the n and an API key"
+        )
+    else:
+        getWeatherAPIInfo(sys.argv[1], 0, 0)
